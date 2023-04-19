ce1100f07dd6757d088353425277727c62c4ed8f
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-19 21:57:52 +0000

    refactoring code

--- src/user/controller.ts
+++ src/user/controller.ts
@@ -44,17 +44,17 @@ export const login = async (req: Request, res: Response) => {
 		if (isValidate) {
 			return res.status(400).json({ message: isValidate })
 		}
-		const user = await prisma.user.findUnique({ where: { email } })
+		const user = await prisma.user.findUnique({ where: { email } });
 		if (!user) {
-			return res.status(400).json({ message: "email or passwor is not correct" });
+			return res.status(404).json({ message: "email or passwor is not correct" });
 		}
 		const isMatch = bcrypt.compareSync(password, user.password);
-		const token = generateToken({ id: user.id, name: user.name })
 		if (isMatch) {
+			const token = generateToken({ id: user.id, name: user.name })
 			return res.status(200).json({ token });
-		} else {
-			return res.status(400).json({ message: "email or passwor is not correct" });
 		}
+		return res.status(404).json({ message: "email or passwor is not correct" });
+
 	} catch (error) {
 		return res.status(500).json({ message: "server was crashed", error });
 	}
